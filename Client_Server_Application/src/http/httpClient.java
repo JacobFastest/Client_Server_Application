f44264c72b421a2358582506b7b4f4236f620b77
Jacob Fast <jacobfast@vt.edu>
2024-03-13 23:36:52 -0400

    Comments
    Added comments to to describe what parts of the code do. Also reformatted some messages printed by the client

--- Client_Server_Application/src/http/httpClient.java
+++ Client_Server_Application/src/http/httpClient.java
@@ -1,26 +1,38 @@
-/**
- * 
- */
 package http;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 /**
+ * The below class has the user input the hostname and port number they are
+ * trying to connect to.
+ * Once they connect they can they fetch a html file from the Server.
+ * After the client receives the file it ends the connection to the server.
  * 
+ * @author Jacob Fast
+ * @version 1.0 2024-03-13
  */
 public class httpClient {
+
     public static void main(String[] args) throws IOException {
+        // Check if the user input the correct number of arguments, if not exit
         if (args.length != 2) {
             System.err.println(
                 "Usage: java httpClient <host name/IPAddress> <port number>");
             System.exit(1);
         }
 
+        // Store the hostName and port number into local variables
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
 
+        // The below try with, attempts to create a new socket from the given
+        // hostName and portnumber
+        // It also makes Printwriter to write to the socket and BufferedReader
+        // to read from the socket
+        // Similarly it makes a BufferedReader on the System's input which
+        // allows
+        // the user to input the name of the file
         try (Socket httpSocket = new Socket(hostName, portNumber);
             PrintWriter out = new PrintWriter(httpSocket.getOutputStream(),
                 true);
@@ -29,58 +41,86 @@ public class httpClient {
 
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(
                 System.in));
-            String fromUser;
+
+            // Below string is used to store what the server sends
             String fromServer;
 
-            Scanner s = new Scanner(System.in);
+            // Ask the user for the name of the file
             System.out.println("Enter the name of the html file:");
-            String html = s.nextLine();
+            String html = stdIn.readLine(); // Get the input
+
+            // Add correct arguments that the server is expectings
             String get = "GET " + "/" + html + " HTTP/1.0";
-            System.out.println("Client: " + get);
-            out.println(get);
+            System.out.println("Sent out the request: "); // This is used to show what the Client is sending to the server
+            System.out.println(get);
+            
+            out.println(get); // Send the message to the server
 
-            boolean fileFound = false;
-
+            // The below while loops reads the response from the server and
+            // based on the response from the server does the correct action
             while ((fromServer = in.readLine()) != null) {
-                System.out.println("Server: " + fromServer);
+                System.out.println("Received response from server:");  
+                System.out.println(fromServer); // Used to tell the user what the server has sent back
+                
+                // If the response is Bad Request tell the user that their input
+                // file name was improperly formatted
                 if (fromServer.equals("HTTP/1.0 400 Bad Request")) {
                     System.out.println(
-                        "The html request was improperly formatted");
+                        "The html request was improperly formatted.");
                     break;
                 }
+                // If the response is 404 tell the user that the inputed file
+                // could not be found
                 else if (fromServer.equals("HTTP/1.0 404 Not Found")) {
-                    System.out.println("The file could not be found");
+                    System.out.println("The file could not be found on the server!");
                     break;
                 }
+                // If the server found the file either create a new file with
+                // the same information or if the file already exists on the
+                // user's machine do nothing
                 else if (fromServer.equals("HTTP/1.0 200 OK")) {
-                    int linen = 0;
-                    while (linen < 2) {
+                    // Below loop prints out the content length and the empty line which should not be apart of the fiel
+                    for (int i = 0; i < 2; i++) {
                         fromServer = in.readLine();
-                        System.out.println("server: " + fromServer);
-                        linen++;
+                        System.out.println(fromServer);
                     }
 
+                    // Make an instance of the file with the given name 
                     File file = new File(html);
-                    if (file.isFile()) {
-                        break;
+                    // Check if the file already exists on the user's machine in the directory they are in. 
+                    // If it does do nothing
+                    if (!file.isFile()) {
+                        // Create the new file
+                        file.createNewFile();
+                        // Make a FileWrite to write to the new file
+                        FileWriter fr = new FileWriter(file, true);
+                        // Read the lines from the server and add them to the file
+                        while ((fromServer = in.readLine()) != null) {
+                            fr.write(fromServer + System.lineSeparator());
+                        }
+                        // Close the file writer 
+                        fr.close();
                     }
-                    file.createNewFile();
-                    FileWriter fr = new FileWriter(file, true);
-                    // should check if file already exists
-
-                    while ((fromServer = in.readLine()) != null) {
-                        fr.write(fromServer + System.lineSeparator());
+                    else {
+                        System.out.println("A file with the same name already exists in the current directory.");
+                        System.out.println("Double check the name of the file you want to copy.");
                     }
-
-                    fr.close();
                 }
             }
-            System.out.println("Closing I/O and quitting...");
+
+            System.out.println("Closing I/O and quitting..."); // Tell the user
+                                                               // that the
+                                                               // session is
+                                                               // over
         }
+        // Catch possible exceptions thrown by try block
+        // This exception occurs when the code can't find the inputed hostname
         catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
         }
+        // This exception occurs when the I/O from the socket is suddenly
+        // disconnected
         catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to "
                 + hostName);
--- Client_Server_Application/src/http/httpServer.java
+++ Client_Server_Application/src/http/httpServer.java
@@ -3,25 +3,38 @@ package http;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
+/**
+ * The below class acts as a server for a user to fetch an html file from
+ * 
+ * @author Jacob Fast
+ * @version 1.0 2024-03-13
+ */
 public class httpServer {
-    public static void main(String[] args)
-        throws IOException,
-        InterruptedException {
-
+    public static void main(String[] args) throws IOException {
+        // Check if the user input the correct number of arguments, if not exit
         if (args.length != 1) {
             System.err.println("Usage: java <port number>");
             System.exit(1);
         }
 
+        // Store the portNumber in a local variable
         int portNumber = Integer.parseInt(args[0]);
-
+        // Int to remember the number of connections served
         int numConnections = 0;
 
+        // Below loop creates a server socket, with the given port number, and
+        // waits for a user to connect to it
+        // Once a user connects they can attempt to fetch a file
         while (true) {
-            System.out.println(numConnections + " connections served. Accepting new client...");
-            
+            // Print out the number of connections served
+            System.out.println(numConnections
+                + " connections served. Accepting new client...");
+
+            // The below try with attempts to create a new server socket from
+            // the given port number.
+            // It also create a PrintWriter and BufferedReader for writing and
+            // reading to the socket.
             try (ServerSocket serverSocket = new ServerSocket(portNumber);
                 Socket clientSocket = serverSocket.accept();
                 PrintWriter out = new PrintWriter(clientSocket
@@ -29,46 +42,67 @@ public class httpServer {
                 BufferedReader in = new BufferedReader(new InputStreamReader(
                     clientSocket.getInputStream()));) {
 
-                String inputLine, outputLine;
-
+                // Use for storing the message sent by the client
+                String inputLine;
                 inputLine = in.readLine();
-                // Use delimiting method used in class to break up input string
+
+                // Split up input string into parts based on where the spaces
+                // occur
+                // Used to check if the Client formated the request correctly
                 String[] arr = inputLine.split("\\s{1,}");
+                // If the client formatted the request correctly check to see if
+                // the server has the file
                 if (arr.length == 3 && arr[0].equals("GET") && arr[2].equals(
                     "HTTP/1.0")) {
-                    // Check if file is on computer if not throw 404
-                    // Might need to add path to arr[1]
-                    String workingDirectory = System.getProperty("user.dir");
+                    String workingDirectory = System.getProperty("user.dir"); // Gets
+                                                                              // path
+                                                                              // for
+                                                                              // file
+                    // Created a file instance based on the Clients request and
+                    // the directory that server is in
                     File f = new File(workingDirectory, arr[1]);
+                    // If the file exists and is not a directory send to the
+                    // Client
+                    // If not tell the
                     if (f.exists() && !f.isDirectory()) {
+                        // Create a scanner to read the file
                         Scanner fileReader = new Scanner(f);
+                        // Tell the Client we found the file
                         out.println("HTTP/1.0 200 OK");
+                        // Get the file length and send it to the Client
                         long fileLength = f.length();
                         out.println("Content-Length: " + Long.toString(
                             fileLength));
                         out.println(System.lineSeparator());
 
+                        // While the are still lines in the file read them and
+                        // send them to the Client
                         while (fileReader.hasNextLine()) {
                             out.println(fileReader.nextLine());
                         }
+                        // Close the scanner
                         fileReader.close();
                     }
+                    // Tells the Client that the file could not be found
                     else {
                         out.println("HTTP/1.0 404 Not Found");
                     }
                 }
+                // If the client didn't format the request correctly tell them
                 else {
                     out.println("HTTP/1.0 400 Bad Request");
                 }
 
             }
+            // Catch the case in which a port is already in use
             catch (IOException e) {
                 System.out.println(
                     "Exception caught when trying to listen on port "
                         + portNumber + " or listening for a connection");
                 System.out.println(e.getMessage());
             }
-            numConnections++;
+            numConnections++; // After the end of the connection increase the
+                              // number of connections
         }
     }
 }
